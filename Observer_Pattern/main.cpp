ba1e22ff9b70e3ce560af19bf994d9862accd375
michalkalinowski- <michal.kalinowski@me.com>
2015-01-22 13:51:46 +0100

    Initial Commit

---
+++ Observer_Pattern/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Observer_Pattern
+//
+//  Created by MAN Team on 22/01/15.
+//  Copyright (c) 2015 MAN Team. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
